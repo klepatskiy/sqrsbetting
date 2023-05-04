f2c235cc116f0bac86ec6076b983d74a384f08fd
klepa <nklepatskiy@gmail.com>
2023-05-04 16:01:44 +0300

    master create skeleton project symfony

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes) {
-    $apiVersionPrefix = '/api/v1/bettingapi';
+    $apiVersionPrefix = '/api/v1';
 
     $routes->import(__DIR__ . '/../src/UI/Http/Controller/', 'annotation')
         ->prefix("$apiVersionPrefix/")
