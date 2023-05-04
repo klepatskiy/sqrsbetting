30e8838a8c22070e8a0ed862c9dde1357596d511
klepa <nklepatskiy@gmail.com>
2023-05-04 17:24:04 +0300

    master create skeleton project symfony

--- rector.php
+++ rector.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Rector\Symfony\Rector\Class_\ChangeFileLoaderInExtensionAndKernelRector;
 use Rector\Config\RectorConfig;
 
-return function (RectorConfig $rectorConfig): void {
+return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(ChangeFileLoaderInExtensionAndKernelRector::class, [
         ChangeFileLoaderInExtensionAndKernelRector::FROM => 'yaml',
         ChangeFileLoaderInExtensionAndKernelRector::TO => 'php',
