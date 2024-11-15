468a65ad4f80df85a3d4d9f95b6c8e969405c9c2
klepa <nklepatskii@marfa-tech.com>
2024-11-15 17:49:08 +0300

    upd to symfony 64

--- src/Application/Exception/NotFoundAppException.php
+++ src/Application/Exception/NotFoundAppException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Application\Exception;
 
-class NotFoundAppException extends AppException
+final class NotFoundAppException extends AppException
 {
     public function __construct(string $message)
     {
